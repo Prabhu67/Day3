42a62f8764edcb115d6d807332f66f4d5a2ce395
Prabhu <123736381+Prabhu67@users.noreply.github.com>
2023-02-24 12:59:16 +0530

    Add files via upload

---
+++ day3/resume.js
@@ -0,0 +1,53 @@
+let myResume={
+    "basics": {
+      "name": "Prabhu",
+      "email": "p.prabhu.pb@gamil.com",
+      "phone": 9688720520,
+      "degree": "BBA",
+      "location": {
+        "address": "7/56,west street,Thalavoi",
+        "postalCode": 621730,
+        "city": "Sendurai",
+        "state": "Tamilnadu",
+        "country": "India"
+      },
+      "profiles": [
+        {
+          
+          "github":"https://github.com/Prabhu67"
+        }
+      ]
+    },
+    "work": [
+      {
+        "company": "vee Tech",
+        "position": "Aadhar Enrollment Operater",
+        "startDate": "2017-12-23",
+        "endDate": "2023-01-23",
+        
+      },
+    ],
+    "education": [
+      {
+        "institution": "Alagappa Univercity ",
+        "department": "BBA",
+        "studyType": "Distace Education",
+        "batch start year": 2019,
+        "batch end year": 2022,
+        "gpa": 6.21,
+      }
+    ],
+    
+    "languages": [
+      {
+        "language": "Tamil,Enlish",
+      }
+    ],
+    "interests": [
+      {
+        "name": "cricket,youtuber",
+      }
+    ]
+  }
+  console.log(myResume);
+  
